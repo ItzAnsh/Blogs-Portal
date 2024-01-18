2b2d9bfce1ffd06db0222e350a75577328b058ed
ItzAnsh <anshbhasin3723@gmail.com>
2024-01-18 10:36:45 +0530

    New files

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,47 @@
 const express = require("express");
 const app = express();
-const port = 4000;
-const connectDB = require("./config/database");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const cors = require("cors");
-const errorHandler = require("./middlewares/error");
+const multer = require("multer");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const commentRoute = require("./routes/comments");
+dotenv.config();
 
-// import Routes
-const UserRoutes = require("./Routes/User.route");
-const BlogRoutes = require("./Routes/Blogs.route");
-const UserBlogRoutes = require("./Routes/UserBlogs.route");
+// console.log(process.env.MONGODB_URI);
 
-// middlewares
-// const JWTDecoder = require("./middlewares/JWTDecoder");
+//database
+const connectDB = async () => {
+	try {
+		await mongoose.connect(process.env.MONGODB_URI);
+		console.log("database is connected successfully!");
+	} catch (err) {
+		console.log(err);
+	}
+};
 
+//middlewares
+dotenv.config();
 app.use(express.json());
-app.use(cors());
+app.use("/images", express.static(path.join(__dirname, "/images")));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cookieParser());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/comments", commentRoute);
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
+const upload = multer({ dest: "images/" });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+	console.log("reached image req");
+	res.status(200).json("Image has been uploaded successfully!");
 });
-app.use(errorHandler);
-app.use("/users", UserRoutes);
-app.use("/blogs", BlogRoutes);
-app.use("/usersblogs", UserBlogRoutes);
 
-app.listen(port, () => {
-	console.log(`running on port ${port}`);
+app.listen(process.env.PORT, () => {
 	connectDB();
+	console.log("app is running on port " + process.env.PORT);
 });
